266f1a343fc03766eb2dd167018fe03ba1819cc0
cruda-K <michellegentile04@gmail.com>
2026-07-28 17:26:02 +0200

    finito alg_simple; da debuggare

--- alg_simple.c
+++ alg_simple.c
@@ -1,5 +1,21 @@
 #include "push_swap.h"
 
+int stack_size(t_list *stack)
+{
+    int     size;
+    t_list  *current;
+
+    size = 0;
+    current = stack;
+
+    while (current != NULL)
+    {
+        size++;
+        current = current->next;
+    }
+    return (size);
+}
+
 int    find_min_index(t_list *stack)
 {
     int min;
@@ -20,8 +36,8 @@ int    find_min_index(t_list *stack)
             min = current->value;
             min_pos = pos;
         }
-        return (min_pos);
     }
+    return (min_pos);
 }
 
 void move_to_top(t_list **a)
@@ -31,7 +47,7 @@ void move_to_top(t_list **a)
     int moves;
 
     min_pos = find_min_index(*a);
-    size = stack_size(*a); \\??
+    size = stack_size(*a);
     if (min_pos <= size / 2)
     {
         while (min_pos > 0)
@@ -45,8 +61,39 @@ void move_to_top(t_list **a)
         moves = size - min_pos;
         while (moves > 0)
         {
-            rra(stack);
+            rra(a);
             moves--;
         }
     }
+}
+
+int is_sorted(t_list *stack)
+{
+    t_list *current;
+    
+    if (stack == NULL || stack->next == NULL)
+        return (1);
+
+    current = stack;
+    while (current->next != NULL)
+    {
+        if (current->value > current->next->value)
+            return (0);
+
+        current = current->next;
+    }
+    return (1);
+}
+
+void simple_sort(t_list **a, t_list **b)
+{
+    if (is_sorted(*a))
+        return;
+    while (*a != NULL)
+    {
+        move_to_top(a);
+        pb(a, b);
+    }
+    while (*b != NULL)
+        pa(a, b);
 }
